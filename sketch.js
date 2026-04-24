5e72cbf8c58960681795e686bf903e391c168fb3
Bat67-png <7020180@spsd.sk.ca>
2026-04-24 11:02:12 -0600

    sprite idle animation added

--- sketch.js
+++ sketch.js
@@ -6,6 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 let spritesheet;
+let frame;
+let frameSwitchTime = 300;
+let lastFrameSwap = 0;
+let frameSwitch = false;
+const FINNFRAMES = 28;
 
 function preload() {
   spritesheet = loadImage("sprites/FinnSprite.png");
@@ -13,9 +18,35 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frame = 0;
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 50);
+  animatedSprite();
+  image(spritesheet, mouseX, mouseY, spritesheet.width/28, spritesheet.height, frame, 0, spritesheet.width/28, spritesheet.height);
+}
+
+function keyPressed() {
+  if (key === "n") {
+    frame += spritesheet.width/28;
+  }
+}
+
+function animatedSprite() {
+  if (millis() > lastFrameSwap + frameSwitchTime) {
+    frameSwitch = !frameSwitch;
+    lastFrameSwap = millis();
+  }
+
+  if (frameSwitch) {
+    if (frame < FINNFRAMES) {
+      frame += spritesheet.width/28;
+      frameSwitch = !frameSwitch;
+    }
+    else {
+      frame = 0;
+      frameSwitch = !frameSwitch;
+    }
+  }
 }
