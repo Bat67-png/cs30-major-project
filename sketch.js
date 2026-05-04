8c6d15d972cbe1f360ef2038ac0c72c92f875347
Bat67-png <7020180@spsd.sk.ca>
2026-05-04 11:02:09 -0600

    tried to use p5.play but failed

--- sketch.js
+++ sketch.js
@@ -2,72 +2,102 @@
 // Bat-Erdene Lkhagvasuren
 // April 22 2026
 
-let finn;
+// let finn;
 
-function preload() {
-  finn = loadImage("sprites/FinnSprite.png");
-  idleFinn = loadImage("sprites/FinnSprite_idle.png");
-}
+// function preload() {
+//   finnImg = loadImage("sprites/FinnSprite.png");
+// }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  finn = new Sprite(width / 2, height / 2, finn, 28);
-}
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   finn = new Sprite();
+//   finn.addAni();
+// }
 
-function draw() {
-  background(220);
+// function draw() {
+//   background(220);
 
-  finn.update();
-  finn.display();
-}
+//   finn.update();
+//   finn.display();
+// }
 
-class Sprite {
-  constructor(x, y, image, frameCount) {
-    this.x = x;
-    this.y = y;
-    this.image = image;
-    this.frameCount = frameCount;
-    this.speed = 1;
+// class Sprite {
+//   constructor(x, y, image, frameCount) {
+//     this.x = x;
+//     this.y = y;
+//     this.image = image;
+//     this.frameCount = frameCount;
+//     this.speed = 1;
 
-    this.frame = 0;
-    this.frameDelay = 300; // milliseconds
-    this.lastFrameTime = 0;
-    this.state = "idle";
-  }
+//     this.frame = 0;
+//     this.frameDelay = 300; // milliseconds
+//     this.lastFrameTime = 0;
+//     this.state = "idle";
+//   }
 
-  update() {
-    // control animation speed
-    if (millis() > this.lastFrameTime + this.frameDelay && this.state === "idle") {
-      for (let i = 0; i < 9; i++) {
-        this.frame = (this.frame + 1) % this.frameCount;
-      }
-      this.frame = 0;
-      this.lastFrameTime = millis();
-    }
+//   update() {
+//     // control animation speed
+//     if (millis() > this.lastFrameTime + this.frameDelay && this.state === "idle") {
+//       for (let i = 0; i < 9; i++) {
+//         this.frame = (this.frame + 1) % this.frameCount;
+//       }
+//       this.frame = 0;
+//       this.lastFrameTime = millis();
+//     }
 
-    // movements: left, right
-    if (keyIsDown(68)) {
-      this.x += this.speed;
-    }
-    if (keyIsDown(65)) {
-      this.x -= this.speed;
-    }
-  }
+//     // movements: left, right
+//     if (keyIsDown(68)) {
+//       this.x += this.speed;
+//     }
+//     if (keyIsDown(65)) {
+//       this.x -= this.speed;
+//     }
+//   }
 
-  display() {
-    let frameWidth = this.image.width / this.frameCount;
-    let frameHeight = this.image.height;
-    noSmooth();
-    image(
-      this.image,
-      this.x,
-      this.y,
-      frameWidth,
-      frameHeight,
-      this.frame * frameWidth, // crop X
-      0,                       // crop Y
-      frameWidth,
-      frameHeight
-    );
-  }
-}
+//   display() {
+//     let frameWidth = this.image.width / this.frameCount;
+//     let frameHeight = this.image.height;
+//     noSmooth();
+//     image(
+//       this.image,
+//       this.x,
+//       this.y,
+//       frameWidth,
+//       frameHeight,
+//       this.frame * frameWidth, // crop X
+//       0,                       // crop Y
+//       frameWidth,
+//       frameHeight
+//     );
+//   }
+// }
+
+// createCanvas();
+// world.gravity.y = 10;
+
+// let ball = new Sprite();
+// ball.x = halfWidth - 200;
+// ball.y = halfHeight - 200;
+// ball.diameter = 50;
+
+// let groundA = new Sprite();
+// groundA.x = halfWidth - 120;
+// groundA.width = 200;
+// groundA.rotation = 20;
+// groundA.physics = STATIC;
+
+// let groundB = new Sprite();
+// groundB.x = halfWidth + 120;
+// groundB.width = 200;
+// groundB.rotation = -20;
+// groundB.physics = STATIC;
+
+// function update() {
+// 	background('skyblue');
+
+// 	textAlign(CENTER);
+// 	textSize(20);
+// 	text('click to jump!', halfWidth, halfHeight - 100);
+
+// 	if (mouse.presses()) ball.vel.y = -5;
+// }
