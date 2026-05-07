0f012caebaf576cdd738164f568a41036a00f4be
Bat67-png <7020180@spsd.sk.ca>
2026-05-07 10:23:45 -0600

    some attempts to add motions

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 
 let finn;
 let hearts;
+let state = "idle";
 
 function preload() {
   finnImg = loadImage("sprites/FinnSprite.png");
@@ -41,7 +42,7 @@ class Sprite {
   }
 
   update() {
-    // control animation speed
+    // control animation speed ++ to make the animation work so that it shows different animations for different states
     if (millis() > this.lastFrameTime + this.frameDelay && this.state === "idle") {
       for (let i = 0; i < 9; i++) {
         this.frame = (this.frame + 1) % this.frameCount;
@@ -57,8 +58,17 @@ class Sprite {
       this.x -= this.speed;
     }
 
-    if (this.y > 100) {
-      this.y -= this.gravity;
+    if (this.y > height && this.state === "jump") {
+      this.y += this.gravity;
+      if (this.y >= height) {
+        this.state = "idle";
+      }
+    }
+    if (keyIsDown(87)) {
+      for (let i = 0; i < 5; i++) {
+        this.y --;
+      }
+      this.state = "jump";
     }
 
   }
@@ -80,7 +90,6 @@ class Sprite {
       frameHeight
     );
     stroke("red");
-    
   }
 }
 
