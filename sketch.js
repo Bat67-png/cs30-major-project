44f0bc90b443bed3dd34f61716a789a5d936e493
Bat67-png <7020180@spsd.sk.ca>
2026-05-25 11:02:24 -0600

    some uptadedes

--- sketch.js
+++ sketch.js
@@ -1,171 +1,197 @@
-// Action platformer game
-// Bat-Erdene Lkhagvasuren
-// April 22 2026
+const { Engine, Bodies, Composite } = Matter;
 
-// let finn;
-// let hearts;
-// let state = "idle";
-// let hit = false;
-// let gate = false;
-
-// function preload() {
-//   finnImg = loadImage("sprites/FinnSprite.png");
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   finn = new Sprite(width/2, height/2, finnImg, 28);
-//   hearts = 5;
-// }
-
-
-
-// function draw() {
-//   background(220);
-
-//   finn.update();
-//   finn.display();
-
-//   text(hearts, 0, 50);
-//   rect(100, 100, 80, 30);
-//   rect(mouseX, mouseY, 50, 75);
-
-//   hit = collideRectRect(100, 100, 80, 30, mouseX, mouseY, 50, 75);
-
-//   stroke(hit ? color('red') : 100);
-//   print('colliding?', hit);
-
-//   // debounce so that only one live gets reduced
-//   if (hit && gate === false) {
-//     hearts--;
-//     gate = true;
-//   }
-
-//   if (!hit) {
-//     gate = false;
-//   }
-  
-// }
-
-// class Sprite {
-//   constructor(x, y, image, frameCount) {
-//     this.x = x;
-//     this.y = y;
-//     this.image = image;
-//     this.frameCount = frameCount;
-//     this.speed = 1;
-//     this.gravity = 5;
-
-//     this.frame = 0;
-//     this.frameDelay = 300; // milliseconds
-//     this.lastFrameTime = 0;
-//     this.state = "idle";
-//   }
-
-//   update() {
-//     // control animation speed ++ to make the animation work so that it shows different animations for different states
-//     if (millis() > this.lastFrameTime + this.frameDelay && this.state === "idle") {
-//       for (let i = 0; i < 9; i++) {
-//         this.frame = (this.frame + 1) % this.frameCount;
-//       }
-//       this.lastFrameTime = millis();
-//     }
-
-//     // movements: left, right
-//     if (keyIsDown(68)) {
-//       this.x += this.speed;
-//     }
-//     if (keyIsDown(65)) {
-//       this.x -= this.speed;
-//     }
-
-//     if (this.y > height && this.state === "jump") {
-//       this.y += this.gravity;
-//       if (this.y >= height) {
-//         this.state = "idle";
-//       }
-//     }
-//     if (keyIsDown(87)) {
-//       for (let i = 0; i < 5; i++) {
-//         this.y --;
-//       }
-//       this.state = "jump";
-//     }
-
-//   }
-
-//   display() {
-//     let frameWidth = this.image.width / this.frameCount;
-//     let frameHeight = this.image.height;
-//     const FINNWIDTH = frameWidth - 15;
-//     const FINNHEIGHT = frameHeight - 10;
-//     rect(this.x + 5, this.y + 5, FINNWIDTH, FINNHEIGHT);
-//     noSmooth();
-//     image(
-//       this.image,
-//       this.x,
-//       this.y,
-//       frameWidth,
-//       frameHeight,
-//       this.frame * frameWidth, // crop X
-//       0,                       // crop Y
-//       frameWidth,
-//       frameHeight
-//     );
-//   }
-// }
-
-/*
------ Coding Tutorial by Patt Vira ----- 
-Name: Intro to matter.js (with p5.js)
-Video Tutorial: https://youtu.be/cLXNxn5N-2Y
-
-Connect with Patt: @pattvira
-https://www.pattvira.com/
-----------------------------------------
-*/
-
-
-const {Engine, Body, Bodies, Composite} = Matter;
 let engine;
-let circleX = 120;
-let circleY = 123;
 let balls = [];
-let ball;
-let radius = 50;
 let ground;
+let radius = 30;
+
+let finn;
+let finnImg;
+let mcBox;
+
+let hearts = 5;
+let hit = false;
+let gate = false;
+
+function preload() {
+  finnImg = loadImage("sprites/FinnSprite.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // Matter.js engine
   engine = Engine.create();
 
-  ground = Bodies.rectangle(100, 700, 400, 10, {isStatic: true});
+  ground = Bodies.rectangle(
+    width / 2,
+    height - 50,
+    width,
+    20,
+    { isStatic: true }
+  );
 
   Composite.add(engine.world, ground);
+
+  // Finn sprite
+  finn = new Sprite(width / 2, height / 2, finnImg, 28);
 }
 
 function draw() {
-  background("white");
-  Engine.update(engine); // Updates the engine 60 times per second
+  background(220);
 
+  // Update physics engine
+  Engine.update(engine);
 
+  // ----------------------------
+  // FINN
+  // ----------------------------
+  finn.update();
+  finn.display();
 
+  // Hearts display
+  fill(0);
+  textSize(32);
+  text("Hearts: " + hearts, 20, 50);
+
+  // ----------------------------
+  // COLLISION TEST RECT
+  // ----------------------------
+  fill("blue");
+  rect(100, 100, 80, 30);
+
+  fill("green");
+  rect(mouseX, mouseY, 50, 75);
+
+  hit = collideRectRect(
+    100,
+    100,
+    80,
+    30,
+    mouseX,
+    mouseY,
+    50,
+    75
+  );
+
+  stroke(hit ? color("red") : 100);
+
+  // Lose one heart only once per touch
+  if (hit && gate === false) {
+    hearts--;
+    gate = true;
+  }
+
+  if (!hit) {
+    gate = false;
+  }
+
+  // ----------------------------
+  // MATTER.JS BALLS
+  // ----------------------------
   noStroke();
   fill("red");
-  for (ball of balls) {
+
+  for (let ball of balls) {
     circle(ball.position.x, ball.position.y, radius * 2);
   }
-  
-  fill("black");
-  rect(ground.position.x, ground.position.y, 400, 10);
 
+  // Ground
+  fill("black");
+  rectMode(CENTER);
+  rect(
+    ground.position.x,
+    ground.position.y,
+    width,
+    20
+  );
 }
 
 function mousePressed() {
-  ball = Bodies.circle(mouseX, mouseY, radius);
+  let ball = Bodies.circle(mouseX, mouseY, radius);
+
   Composite.add(engine.world, ball);
+
   balls.push(ball);
 }
 
+// ====================================
+// SPRITE CLASS
+// ====================================
 
+class Sprite {
+  constructor(x, y, image, frameCount) {
+    this.x = x;
+    this.y = y;
 
+    this.image = image;
+    this.frameCount = frameCount;
+
+    this.speed = 5;
+    this.gravity = 5;
+
+    this.frame = 0;
+    this.frameDelay = 120;
+    this.lastFrameTime = 0;
+
+    this.state = "idle";
+  }
+
+  update() {
+
+    // Animation
+    if (
+      millis() > this.lastFrameTime + this.frameDelay
+    ) {
+      this.frame =
+        (this.frame + 1) % this.frameCount;
+
+      this.lastFrameTime = millis();
+    }
+
+    // Movement
+    if (keyIsDown(68)) {
+      this.x += this.speed;
+    }
+
+    if (keyIsDown(65)) {
+      this.x -= this.speed;
+    }
+
+    // Jump
+    if (keyIsDown(87)) {
+      this.y -= 5;
+      this.state = "jump";
+    }
+
+    // Fake gravity
+    if (this.y < height / 2) {
+      this.y += this.gravity * 0.5;
+    }
+  }
+
+  display() {
+
+    let frameWidth =
+      this.image.width / this.frameCount;
+
+    let frameHeight =
+      this.image.height;
+
+    noSmooth();
+
+    image(
+      this.image,
+      this.x,
+      this.y,
+      frameWidth,
+      frameHeight,
+
+      this.frame * frameWidth,
+      0,
+
+      frameWidth,
+      frameHeight
+    );
+  }
+}
